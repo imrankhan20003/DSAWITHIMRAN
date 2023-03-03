9515bc72a5278d429bfb81833faf89f111e4b5d9
imran2003 <imranbaloch5794@gmail.com>
2023-03-03 12:12:42 +0500

    my

---
+++ imran.py
@@ -0,0 +1,11 @@
+print("\t""my profile")
+name="umar"
+print("name:", name)
+last_name="raza"
+print("last_name:", last_name)
+programe="BS_RS&GISC"
+print("programe:", programe)
+age="birth=16/10/2001"
+print("age", age)
+semester="4th"
+print("semester:", semester)

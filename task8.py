fbc3a132509845fc5b581f2a3b031d7f719fd480
imrankhan20003 <imranbaloch5794@gmail.com>
2023-03-16 14:42:01 +0500

    basic python

---
+++ task8.py
@@ -0,0 +1,6 @@
+print(2+110+22)
+print(2/500+255-45)
+print("Welcome to Data structure & Algorithems Course(ucc-227205")
+Course_name = "DSA Python Class"
+print(Course_name)
+Course_name=input("Enter your course name:")

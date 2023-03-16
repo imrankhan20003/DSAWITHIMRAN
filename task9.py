863e8438e02178bc295b83bfaa93a0de6f6f5294
imrankhan20003 <imranbaloch5794@gmail.com>
2023-03-16 15:14:41 +0500

    while loop

--- task9.py
+++ task9.py
@@ -1,13 +1,6 @@
-marks = input("Enter your marks")
-if marks <25:
-    print"F"
-elif marks>=25 and marks<45:
-    print"E"
-elif marks>=45 and marks<50:
-    print"D"
-elif marks>=50 and marks<70:
-    print"C"
-elif marks>=70 and marks<80:
-    print"B"
-else:
-    print"A"606
+i = 1
+while i < 60:
+    print(i)
+    if  i == 6:
+        break
+    i +=1

e86241c767af6fb80005de4a7f3b5b043c373ccc
imrankhan20003 <imranbaloch5794@gmail.com>
2023-03-16 14:55:38 +0500

    conditional

---
+++ task9.py
@@ -0,0 +1,13 @@
+marks = input("Enter your marks")
+if marks <25:
+    print"F"
+elif marks>=25 and marks<45:
+    print"E"
+elif marks>=45 and marks<50:
+    print"D"
+elif marks>=50 and marks<70:
+    print"C"
+elif marks>=70 and marks<80:
+    print"B"
+else:
+    print"A"606

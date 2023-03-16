dfd0a013d6d676e7200d167476713ed2c302d03b
imrankhan20003 <imranbaloch5794@gmail.com>
2023-03-16 14:32:55 +0500

    nested condition

---
+++ task 7.py
@@ -0,0 +1,9 @@
+a=0
+if>0 and a %2==0:
+    print('A is an even number and positive')
+elif a>0 and% 2!= 0:
+    print('A is a positive number')
+elif a== 0:
+    print('A is zero')
+else:
+    print('A is a negative number')

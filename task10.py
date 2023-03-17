b73b4f6daf2087ec8bfbd16a60497158fabb3025
imrankhan20003 <imranbaloch5794@gmail.com>
2023-03-17 10:45:22 +0500

    basic python

---
+++ task10.py
@@ -0,0 +1,9 @@
+number_1=25
+number_2=30
+
+print(number_1 > number_2)
+print(number_1 < number_2)
+print(number_1 == number_2)
+print(number_1 != number_2)
+print(number_1 >= number_2)
+print(number_1 <= number_2)

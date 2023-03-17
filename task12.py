278975e84ea5eb4ee7fc4a0fc57abf3981b0a145
imrankhan20003 <imranbaloch5794@gmail.com>
2023-03-17 12:30:57 +0500

    class work

---
+++ task12.py
@@ -0,0 +1,9 @@
+list1=[['imran','jawad'],'khan']
+list2=["imran",'211101016','jawad']
+list3=[['gender','male']]
+print['name',list1[0][0]]
+print(list2)
+print('gender',list3)
+list4= ['4','b','c','d','e','f']
+list4[1:3]----['b','c']
+print(list4)

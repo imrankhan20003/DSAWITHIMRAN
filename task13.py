3818eb3711206b86bee6290b845e15ed818be0da
imrankhan20003 <imranbaloch5794@gmail.com>
2023-03-17 12:31:22 +0500

    class work

---
+++ task13.py
@@ -0,0 +1,10 @@
+print("Data Structure with python")
+age =int(input("What is your age:"))
+print(age)
+print(type(age))
+name = input("what is your name:")
+print(name)
+print(type(name))
+x=2*2**3
+print(x)
+del(age)
